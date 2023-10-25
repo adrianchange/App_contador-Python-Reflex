a0028855017a5b4b2556b80dc09c55e3a62ef18f
Adrian Duta <adrianchange@gmail.com>
2023-10-25 22:12:26 +0200

    First commit

---
+++ .web/pages/index.js
@@ -0,0 +1,86 @@
+import { Fragment, useContext, useEffect, useRef, useState } from "react"
+import { useRouter } from "next/router"
+import { Event, getAllLocalStorageItems, getRefValue, getRefValues, isTrue, preventDefault, refs, spreadArraysOrObjects, uploadFiles, useEventLoop } from "/utils/state"
+import { EventLoopContext, initialEvents, StateContext } from "/utils/context.js"
+import "focus-visible/dist/focus-visible"
+import { Box, Button, Heading, HStack, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, Text, useColorMode, VStack } from "@chakra-ui/react"
+import NextHead from "next/head"
+
+
+
+export default function Component() {
+  const state = useContext(StateContext)
+  const router = useRouter()
+  const { colorMode, toggleColorMode } = useColorMode()
+  const focusRef = useRef();
+  
+  // Main event loop.
+  const [addEvents, connectError] = useContext(EventLoopContext)
+
+  // Set focus to the specified element.
+  useEffect(() => {
+    if (focusRef.current) {
+      focusRef.current.focus();
+    }
+  })
+
+  // Route after the initial page hydration.
+  useEffect(() => {
+    const change_complete = () => addEvents(initialEvents.map((e) => ({...e})))
+    router.events.on('routeChangeComplete', change_complete)
+    return () => {
+      router.events.off('routeChangeComplete', change_complete)
+    }
+  }, [router])
+
+
+  return (
+    <Fragment>
+  <Fragment>
+  {isTrue(connectError !== null) ? (
+  <Fragment>
+  <Modal isOpen={connectError !== null}>
+  <ModalOverlay>
+  <ModalContent>
+  <ModalHeader>
+  {`Connection Error`}
+</ModalHeader>
+  <ModalBody>
+  <Text>
+  {`Cannot connect to server: `}
+  {(connectError !== null) ? connectError.message : ''}
+  {`. Check if server is reachable at `}
+  {`http://localhost:8000`}
+</Text>
+</ModalBody>
+</ModalContent>
+</ModalOverlay>
+</Modal>
+</Fragment>
+) : (
+  <Fragment/>
+)}
+</Fragment>
+  <VStack spacing={`1em`}>
+  <HStack>
+  <Button onClick={(_e) => addEvents([Event("state.decrement", {})], (_e))} sx={{"bg": "#fef2f2", "color": "#b91c1c", "borderRadius": "lg"}}>
+  {`Decrement`}
+</Button>
+  <Heading sx={{"fontSize": "2em"}}>
+  {state.count}
+</Heading>
+  <Button onClick={(_e) => addEvents([Event("state.increment", {})], (_e))} sx={{"bg": "#ecfdf5", "color": "#047857", "borderRadius": "lg"}}>
+  {`Increment`}
+</Button>
+</HStack>
+</VStack>
+  <NextHead>
+  <title>
+  {`Reflex App`}
+</title>
+  <meta content={`A Reflex app.`} name={`description`}/>
+  <meta content={`favicon.ico`} property={`og:image`}/>
+</NextHead>
+</Fragment>
+  )
+}
